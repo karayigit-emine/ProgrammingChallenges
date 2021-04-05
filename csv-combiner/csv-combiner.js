ec1c26c13c0de4810943fc0ef90612ddc1516c31
William Leung <w.leung.93@gmail.com>
2021-04-05 09:41:07 -0700

    readme

--- csv-combiner/csv-combiner.js
+++ csv-combiner/csv-combiner.js
@@ -50,3 +50,45 @@ const mergeFiles = async (files, mergedFile) => {
 
 mergeFiles(filePaths, "combined.csv");
 
+
+
+//!=====````````````````````````````````````similar to above
+// function concatCSVAndOutput(csvFilePaths, outputFilePath) {
+//   const promises = csvFilePaths.map((path) => {
+//     return new Promise((resolve) => {
+//       const dataArray = [];
+//       return csv
+//           .parseFile(path, {headers: true, escape: "\\"})
+//           .on('data', function(data) {
+//             dataArray.push(data);
+//           })
+//           .on('end', function() {
+//             resolve(dataArray);
+//           });
+//     });
+//   });
+
+//   return Promise.all(promises)
+//       .then((results) => {
+
+//         const csvStream = csv.format({headers: true });
+//         const writableStream = fs.createWriteStream(outputFilePath);
+
+//         writableStream.on('finish', function() {
+//           console.log('DONE!');
+//         });
+
+//         csvStream.pipe(writableStream);
+//         results.forEach((result) => {
+//           result.forEach((data) => {
+//             csvStream.write(data);
+//           });
+//         });
+//         csvStream.end();
+
+//       });
+//     }
+
+// // concatCSVAndOutput(['./fixtures/clothing.csv', './fixtures/accessories.csv'], 'outputfile.csv')
+// concatCSVAndOutput(['./fixtures/clothing.csv', './fixtures/accessories.csv'], 'outputfile.csv')
+//     .then(() => console.log('hi'));
